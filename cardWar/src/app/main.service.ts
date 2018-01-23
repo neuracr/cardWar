d01d51bdaf101d9d442ca60e94e0278eb2291842
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-01-23 16:28:44 +0100

    ça marche mais je sais pas pourquoi. On peut sauvegarder le username

--- cardWar/src/app/main.service.ts
+++ cardWar/src/app/main.service.ts
@@ -4,11 +4,20 @@ import { Player } from './back/player';
 @Injectable()
 export class MainService {
 
-  player: Player = null;
+  player: Player = {
+    id: 0,
+    username: '',
+    score: 0,
+    stack: null,
+  }
   constructor() { }
 
   getPlayer(): Player {
     return this.player;
+  }
+
+  setPlayerUsername(name: string): void {
+    this.player.username = name;
   }
 
 }
