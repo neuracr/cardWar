9a9eeef2611bdbc85826e7dcf394c156403dac79
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-01-23 14:34:36 +0100

    ajout de la route du menu et redirection vers le menu en cas de racine

--- cardWar/src/app/app-routing.module.ts
+++ cardWar/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+];
 
 @NgModule({
   exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
 })
 export class AppRoutingModule {}
