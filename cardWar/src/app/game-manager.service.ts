54e833623c8d5e9d204cf946409c75635e42edb7
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-01-24 15:17:23 +0100

    .

--- cardWar/src/app/game-manager.service.ts
+++ cardWar/src/app/game-manager.service.ts
@@ -31,38 +31,50 @@ export class GameManagerService {
     }
   }
 
-public joinGame(player: Player){
-  console.log("joined game in game manager");
-  if (this.player1 == null){
-    this.player1 = player;
-    //création du bot
-    this.player2 = new Player
-    this.player1.pack = [];
-    this.player2.pack = [];;
+  public joinGame(player: Player){
+    console.log("joined game in game manager");
+    if (this.player1 == null){
+      this.player1 = player;
+      //création du bot
+      this.player2 = new Player
+      this.player1.pack = [];
+      this.player2.pack = [];;
 
-    this.startGame();
-    console.log(this.player1.pack);
-    console.log(this.player2.pack);
-  }
-  //ne sera jamais executé pour l'instant normalement
-  else if (this.player2 == null){
-    this.player2 = player;
-    this.startGame();
+      this.startGame();
+      console.log(this.player1.pack);
+      console.log(this.player2.pack);
+    }
+    //ne sera jamais executé pour l'instant normalement
+    else if (this.player2 == null){
+      this.player2 = player;
+      this.startGame();
 
+    }
+    else{
+      console.log("comment est-on arrivé ici ?");
+    }
   }
-  else{
-    console.log("comment est-on arrivé ici ?");
-  }
-}
 
   public startGame(): void{
     this.shuffle();
     this.deal()
   }
 
-  public playCard(card: Card, player: Player){
+  public playCard(card: Card, player: Player): void {
+    if(this.player1 == player){
+      console.log("received card from player1");
 
+    }
+    else if (this.player2 == player){
+      console.log("received card from player2");
+    }
+    else{
+      console.log("received card from unknown player");
+      return;
+    }
+    //on regarde si l'on a les cartes des deux joueurs pour savoir qui gagne.
   }
+
 
 
 
