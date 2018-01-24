fb9b1e073251ea83baa22ba80587bd68a1eaa9bf
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-01-24 10:42:19 +0100

    je sais pu ce que j'ai ajouté

--- cardWar/src/app/menu-page/menu-page.component.ts
+++ cardWar/src/app/menu-page/menu-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-page.component.css']
 })
 export class MenuPageComponent implements OnInit {
+
+  @Input('title') title: string;
 
   constructor() { }
 
