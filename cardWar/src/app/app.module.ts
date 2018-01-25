f614c09714434bd6e4226a40b4212942fddcab32
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-01-25 11:34:20 +0100

    commit avant intégration frontend

--- cardWar/src/app/app.module.ts
+++ cardWar/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GameComponent } from './game/game.component';
 import { MenuPageComponent } from './menu-page/menu-page.component';
 import { GameManagerService } from './game-manager.service';
 import { PlayerService } from './player.service';
+import { BotService } from './bot.service';
 
 
 @NgModule({
@@ -32,7 +33,7 @@ import { PlayerService } from './player.service';
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [MainService, GameManagerService, PlayerService, ],
+  providers: [MainService, GameManagerService, PlayerService, BotService, ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- cardWar/src/app/back/card.ts
+++ cardWar/src/app/back/card.ts
@@ -1,4 +1,8 @@
 export class Card {
   value: string; // value of the card in {A,2,3,4,5,6,7,8,9,0,J,Q,K}
   color: string; // color of the card in {s,h,d,c}
+
+  public toString(): string{
+    return this.value + this.color;
+  }
 }
